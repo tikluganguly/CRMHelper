ec83c144ed093b08a67980afac3c7105789d8d58
Tiklu Ganguly <tikluganguly@gmail.com>
2017-11-23 12:19:25 +0530

    Added code in the main crm helper

--- Snipt.CRM/CRMHelper.cs
+++ Snipt.CRM/CRMHelper.cs
@@ -12,16 +12,79 @@
 //See the License for the specific language governing permissions and
 //limitations under the License.
 
+using Microsoft.Azure;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Metadata;
+using Microsoft.Xrm.Sdk.Query;
+using Snipt.CRM.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ServiceModel.Description;
 
 namespace Snipt.CRM
 {
     public class CRMHelper
     {
+        public static OrganizationServiceProxy GetOrgService()
+        {
+            var url = new Uri(CloudConfigurationManager.GetSetting("CRM:ServiceUrl"));
+            var cred = new ClientCredentials();
+            cred.UserName.UserName = CloudConfigurationManager.GetSetting("CRM:UserName");
+            cred.UserName.Password = CloudConfigurationManager.GetSetting("CRM:Password");
+            var org = new OrganizationServiceProxy(url, null, cred, null);
+            org.ServiceConfiguration.CurrentServiceEndpoint.Behaviors.Add(new ProxyTypesBehavior());
+            return org;
+        }
 
+        public static List<T> GetList<T>(QueryBase fetchExpression, Func<Entity, T> action)
+        {
+            using (var org = GetOrgService())
+            {
+                var res = org.RetrieveMultiple(fetchExpression);
+                var list = new List<T>();
+                foreach (var ent in res.Entities)
+                {
+                    list.Add(action(ent));
+                }
+                return list;
+            }
+        }
+
+        public static List<T> GetList<T>(string fetchXML, Func<Entity, T> action)
+        {
+            return GetList<T>(new FetchExpression(fetchXML), action);
+        }
+
+        public static Entity GetSingle(string fetchXML)
+        {
+            using (var org = GetOrgService())
+            {
+                var res = org.RetrieveMultiple(new FetchExpression(fetchXML));
+                return res.Entities.FirstOrDefault();
+            }
+        }
+
+        public static List<OptionSet> GetOptions(string optionName)
+        {
+            var options = new List<OptionSet>();
+            using (var org = GetOrgService())
+            {
+                var retrieveOptionSetRequest = new RetrieveOptionSetRequest()
+                {
+                    Name = optionName
+                };
+                var response = (RetrieveOptionSetResponse)org.Execute(retrieveOptionSetRequest);
+
+                foreach (var opt in ((OptionSetMetadata)response.OptionSetMetadata).Options.ToList())
+                {
+                    options.Add(OptionSet.FromOption(opt));
+                }
+            }
+
+            return options;
+        }
     }
 }
